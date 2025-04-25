c88c4be3cdde5782a5eba392c2dbe60a1fc3a774
Robique <robert-cristian.lungu@student.unitbv.ro>
2025-04-25 11:50:44 +0300

    Added password encryption

--- online-shop-robique50-main/shop/src/main/java/ro/msg/learning/shop/security/SecurityConfig.java
+++ online-shop-robique50-main/shop/src/main/java/ro/msg/learning/shop/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -64,19 +65,9 @@ public class SecurityConfig {
         return source;
     }
 
-    @Bean
+   @Bean
     public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return rawPassword.toString().equals(encodedPassword);
-            }
-        };
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
